ad70b8c0ffdc0430f7cdc4bd03e61754ccd1ccb8
Sam Woodman <sam.woodman@noaa.gov>
2024-04-13 00:09:10 +0000

    made another copy, to run thing in 10-directory chunks to try

--- gcp-file-move/file_move.py
+++ gcp-file-move/file_move.py
@@ -151,8 +151,9 @@ def copy_blob_if_new(
         copy_blob(source_bucket, blob_name, 
                   destination_bucket, destination_blob_name)
         
-def copy_blob_client_ifnew(
-    project_name, bucket_name, blob_name, destination_bucket_name, destination_blob_name
+def copy_blob_client(
+    bucket_name, blob_name, destination_bucket_name, destination_blob_name, 
+    project_name = "ggn-nmfs-usamlr-dev-7b99", 
 ):
     """
     Copies a blob from one bucket to another with a new name.
@@ -163,13 +164,13 @@ def copy_blob_client_ifnew(
     source_bucket = storage_client.bucket(bucket_name)
     source_blob = source_bucket.blob(blob_name)
     destination_bucket = storage_client.bucket(destination_bucket_name)
-    if destination_bucket.blob(destination_blob_name).exists(storage.Client(project_name)):
-        return
-    else:
-         blob_copy = source_bucket.copy_blob(
-            source_blob, destination_bucket, destination_blob_name
-            #if_generation_match=destination_generation_match_precondition,
-        )
+    # if destination_bucket.blob(destination_blob_name).exists(storage.Client(project_name)):
+    #     return
+    # else:
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name
+        #if_generation_match=destination_generation_match_precondition,
+    )
     
 
 def move_blob_wrapper(file_old):
